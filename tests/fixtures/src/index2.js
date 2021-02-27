4dd1e6b886f719dd534eb1b4adeb674dcf79a918
soufyakoub <splentercell.1997@gmail.com>
2021-02-27 23:08:54 +0100

    tests: add non-string literals to test fixtures

--- tests/fixtures/src/index2.js
+++ tests/fixtures/src/index2.js
@@ -4,4 +4,5 @@ import font from "@fontsource/open-sans/files/open-sans-all-300-italic.woff";
 export default {
 	default: _default,
 	font,
+	non_string_literals: [1929, /^ab|b$/g, null, false],
 };
