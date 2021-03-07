22025b1ae85c9b83ad1bd8c40651e5b8eb7c4162
soufyakoub <splentercell.1997@gmail.com>
2021-03-07 22:46:14 +0100

    tests: simplify snapshots

--- tests/macros.js
+++ tests/macros.js
@@ -1,25 +1,38 @@
 const { rollup } = require("rollup");
+const fs = require("fs/promises");
 
 function noop() { }
 
-const normalizeChunks = chunks => chunks.map(chunk => {
-	// These properties are inconsistent between platforms.
-	// (absolute paths, end of line chars)
-	const {
-		dynamicImports,
-		facadeModuleId,
-		importedBindings,
-		imports,
-		modules,
-		referencedFiles,
-		code,
-		...rest
-	} = chunk;
+const getChunksSnapshot = output => output.map(chunk => {
+	const whiteList = [
+		"code",
+		"fileName",
+		"name",
+		"isDynamicEntry",
+		"isEntry",
+		"isImplicitEntry",
+		"map",
+	];
 
-	return {
-		...rest,
-		code: code.replace(/\r\n|\n/g, "\n"),
-	};
+	const obj = {};
+	whiteList.forEach(key => {
+		let value = chunk[key];
+		// normalize for windows.
+		if (key === "code") value = value.replace(/\r\n/g, "\n");
+		obj[key] = value;
+	});
+	return obj;
+});
+
+const getAssetsSnapshot = output => output.map(asset => {
+	const whiteList = [
+		"fileName",
+		"name",
+	];
+
+	const obj = {};
+	whiteList.forEach(key => obj[key] = asset[key]);
+	return obj;
 });
 
 module.exports.outputSnapshotMacro = async function (t, options) {
@@ -35,8 +48,21 @@ module.exports.outputSnapshotMacro = async function (t, options) {
 
 	const chunks = output.filter(element => element.type === "chunk");
 	const assets = output.filter(element => element.type === "asset");
-	const normalizedChunks = normalizeChunks(chunks);
+	const chunksSnapshot = getChunksSnapshot(chunks);
+	const assetsSnapshot = getAssetsSnapshot(assets);
 
-	t.snapshot(normalizedChunks);
-	t.snapshot(assets);
+	t.snapshot(chunksSnapshot);
+	t.snapshot(assetsSnapshot);
+
+	for (const asset of assets) {
+		const fixture_path = /\.woff$/.test(asset.name) // font assets from the @fontsource/open-sans package.
+			? `tests/fixtures/node_modules/@fontsource/open-sans/files/${asset.name}`
+			: `tests/fixtures/assets/${asset.name}`;
+
+		const fixture_buffer = await fs.readFile(fixture_path);
+		const asset_buffer = Buffer.from(asset.source);
+		const message = `fixture buffer does not equal asset buffer. asset.fileName='${asset.fileName}'`;
+
+		t.true(fixture_buffer.equals(asset_buffer), message);
+	}
 };
