033b1ccaf2e0e86ff80eb27f6620553dbe9a751b
recursive-beast <splentercell.1997@gmail.com>
2023-08-05 15:06:29 +0100

    style: use 2 space indentation for js related files

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,42 @@
 import { builtinModules } from "module";
-import nodeResolve from '@rollup/plugin-node-resolve';
+import nodeResolve from "@rollup/plugin-node-resolve";
 import ts from "rollup-plugin-ts";
 import transformDefaultExport from "ts-transform-default-export";
 import pkg from "./package.json";
 
 const external = [
-	...Object.keys(pkg.dependencies),
-	// ...Object.keys(pkg.peerDependencies),
-	...builtinModules,
+  ...Object.keys(pkg.dependencies),
+  // ...Object.keys(pkg.peerDependencies),
+  ...builtinModules,
 ];
 
 export default [
-	{
-		input: "src/index.ts",
-		output: {
-			file: pkg.main,
-			format: "cjs",
-			exports: "default",
-			sourcemap: true,
-		},
-		external,
-		plugins: [
-			nodeResolve(),
-			ts({
-				transformers: ({ program }) => ({
-					afterDeclarations: transformDefaultExport(program),
-				}),
-			}),
-		],
-	},
-	{
-		input: "src/index.ts",
-		output: {
-			file: pkg.module,
-			format: "es",
-			sourcemap: true,
-		},
-		external,
-		plugins: [
-			nodeResolve(),
-			ts(),
-		],
-	},
+  {
+    input: "src/index.ts",
+    output: {
+      file: pkg.main,
+      format: "cjs",
+      exports: "default",
+      sourcemap: true,
+    },
+    external,
+    plugins: [
+      nodeResolve(),
+      ts({
+        transformers: ({ program }) => ({
+          afterDeclarations: transformDefaultExport(program),
+        }),
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true,
+    },
+    external,
+    plugins: [nodeResolve(), ts()],
+  },
 ];
--- src/index.ts
+++ src/index.ts
@@ -13,35 +13,35 @@ const PREFIX = `\0${PLUGIN_NAME}:`;
 export type FilterPattern = string | RegExp | (string | RegExp)[];
 
 export interface ExternalAssetsOptions {
-	/** A pattern, or array of patterns, to match files the plugin should ignore. */
-	include: FilterPattern;
-	/** A pattern, or array of patterns, to match files the plugin should operate on. */
-	exclude?: FilterPattern;
-	/** The value will be used as the base directory for resolving patterns. By default it's `process.cwd()`. */
-	resolve?: string;
+  /** A pattern, or array of patterns, to match files the plugin should ignore. */
+  include: FilterPattern;
+  /** A pattern, or array of patterns, to match files the plugin should operate on. */
+  exclude?: FilterPattern;
+  /** The value will be used as the base directory for resolving patterns. By default it's `process.cwd()`. */
+  resolve?: string;
 }
 
 const union = Joi.alternatives(Joi.string(), Joi.object().regex());
 const patternSchema = Joi.alternatives<FilterPattern>(
-	union,
-	Joi.array().items(union).min(1)
+  union,
+  Joi.array().items(union).min(1)
 );
 const optionsSchema = Joi.object<ExternalAssetsOptions>({
-	include: patternSchema.required(),
-	exclude: patternSchema,
-	resolve: Joi.string(),
+  include: patternSchema.required(),
+  exclude: patternSchema,
+  resolve: Joi.string(),
 });
 
 function validate(arg: unknown): ExternalAssetsOptions {
-	const pattern = patternSchema.validate(arg);
+  const pattern = patternSchema.validate(arg);
 
-	if (!pattern.error) return { include: pattern.value };
+  if (!pattern.error) return { include: pattern.value };
 
-	const options = optionsSchema.validate(arg);
+  const options = optionsSchema.validate(arg);
 
-	if (!options.error) return options.value;
+  if (!options.error) return options.value;
 
-	throw new TypeError("Invalid argument");
+  throw new TypeError("Invalid argument");
 }
 
 /**
@@ -57,67 +57,64 @@ function externalAssets(options: ExternalAssetsOptions): Plugin;
 function externalAssets(pattern: FilterPattern): Plugin;
 
 function externalAssets(arg: unknown): Plugin {
-	const { include, exclude, resolve } = validate(arg);
-	const idFilter = createFilter(include, exclude, { resolve });
+  const { include, exclude, resolve } = validate(arg);
+  const idFilter = createFilter(include, exclude, { resolve });
 
-	return {
-		name: PLUGIN_NAME,
+  return {
+    name: PLUGIN_NAME,
 
-		resolveId(source, importer, options) {
-			// skip resolving entry points or imports not introduced by us in the load hook.
-			if (!importer || options.isEntry || !source.startsWith(PREFIX))
-				return null;
+    resolveId(source, importer, options) {
+      // skip resolving entry points or imports not introduced by us in the load hook.
+      if (!importer || options.isEntry || !source.startsWith(PREFIX))
+        return null;
 
-			return {
-				id: source,
-				external: true,
-			};
-		},
+      return {
+        id: source,
+        external: true,
+      };
+    },
 
-		async load(id) {
-			if (!idFilter(id)) return null;
+    async load(id) {
+      if (!idFilter(id)) return null;
 
-			const ref = this.emitFile({
-				type: "asset",
-				name: path.basename(id),
-				source: await fs.readFile(id),
-			});
+      const ref = this.emitFile({
+        type: "asset",
+        name: path.basename(id),
+        source: await fs.readFile(id),
+      });
 
-			return `export * from "${PREFIX}${ref}";
+      return `export * from "${PREFIX}${ref}";
 			export { default } from "${PREFIX}${ref}";
 			`;
-		},
+    },
 
-		async renderChunk(code, chunk) {
-			const context = this;
-			const ast = parse(code, { sourceType: "unambiguous" });
+    async renderChunk(code, chunk) {
+      const context = this;
+      const ast = parse(code, { sourceType: "unambiguous" });
 
-			traverse(ast, {
-				StringLiteral(nodePath) {
-					const node = nodePath.node;
-					const value = node.value;
+      traverse(ast, {
+        StringLiteral(nodePath) {
+          const node = nodePath.node;
+          const value = node.value;
 
-					if (!value.startsWith(PREFIX)) return;
+          if (!value.startsWith(PREFIX)) return;
 
-					const ref = value.slice(PREFIX.length);
-					const assetFileName = context.getFileName(ref);
-					const dir = path.dirname(chunk.fileName);
-					let relative = path.posix.relative(dir, assetFileName);
+          const ref = value.slice(PREFIX.length);
+          const assetFileName = context.getFileName(ref);
+          const dir = path.dirname(chunk.fileName);
+          let relative = path.posix.relative(dir, assetFileName);
 
-					if (
-						!relative.startsWith("./") &&
-						!relative.startsWith("../")
-					) {
-						relative = "./" + relative;
-					}
+          if (!relative.startsWith("./") && !relative.startsWith("../")) {
+            relative = "./" + relative;
+          }
 
-					node.value = relative;
-				},
-			});
+          node.value = relative;
+        },
+      });
 
-			return generate(ast, { sourceMaps: true });
-		},
-	};
+      return generate(ast, { sourceMaps: true });
+    },
+  };
 }
 
 export { externalAssets };
