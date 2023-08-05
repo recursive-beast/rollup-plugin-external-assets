993c8f9d586671b3dbd056dafdc341dc417d0143
recursive-beast <splentercell.1997@gmail.com>
2023-08-05 15:06:29 +0100

    style: use 2 space indentation for js related files

--- release.config.js
+++ release.config.js
@@ -1,23 +1,18 @@
 module.exports = {
-	branches: [
-		"master",
-	],
-	plugins: [
-		"@semantic-release/commit-analyzer",
-		"@semantic-release/release-notes-generator",
-		"@semantic-release/changelog",
-		"@semantic-release/npm",
-		[
-			"@semantic-release/git",
-			{
-				assets: [
-					"package.json",
-					"package-lock.json",
-					"CHANGELOG.md",
-				],
-				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-			}
-		],
-		"@semantic-release/github",
-	],
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
 };
